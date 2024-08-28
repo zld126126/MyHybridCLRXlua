97e887c444ff50d4e868d1be1e4436d0198bf197
zld126126 <zld126126@126.com>
2024-08-28 14:36:36 +0800

    update

--- Assets/HybridCLRData/Generated/AOTGenericReferences.cs
+++ Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -5,8 +5,6 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// {{ AOT assemblies
 	public static readonly IReadOnlyList<string> PatchedAOTAssemblyList = new List<string>
 	{
-		"UnityEngine.CoreModule.dll",
-		"mscorlib.dll",
 	};
 	// }}
 
@@ -14,24 +12,9 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// }} 
 
 	// {{ AOT generic types
-	// System.Action<Entry.MyVec3>
-	// System.Collections.Generic.ArraySortHelper<Entry.MyVec3>
-	// System.Collections.Generic.Comparer<Entry.MyVec3>
-	// System.Collections.Generic.ICollection<Entry.MyVec3>
-	// System.Collections.Generic.IComparer<Entry.MyVec3>
-	// System.Collections.Generic.IEnumerable<Entry.MyVec3>
-	// System.Collections.Generic.IEnumerator<Entry.MyVec3>
-	// System.Collections.Generic.IList<Entry.MyVec3>
-	// System.Collections.Generic.List.Enumerator<Entry.MyVec3>
-	// System.Collections.Generic.List<Entry.MyVec3>
-	// System.Collections.Generic.ObjectComparer<Entry.MyVec3>
-	// System.Collections.ObjectModel.ReadOnlyCollection<Entry.MyVec3>
-	// System.Comparison<Entry.MyVec3>
-	// System.Predicate<Entry.MyVec3>
 	// }}
 
 	public void RefMethods()
 	{
-		// object UnityEngine.GameObject.AddComponent<object>()
 	}
 }
